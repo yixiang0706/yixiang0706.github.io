4bfca1f8d7d6c1e135db9ef53d81777c0a7c69e9
yixiang0706 <97888369+yixiang0706@users.noreply.github.com>
2022-03-30 21:55:47 +0800

    new
    new

--- 自動排課系統/main.js
+++ 自動排課系統/main.js
@@ -15,20 +15,18 @@
 })*/
 
 var Tablestatus = 0;
-var count=1;
+var count = 1;
 $('#select').submit(function () {
-    //alert( $("#month").val());
     if (Tablestatus) {
         $("#coursetable").remove();
         $("#area").append(
             `<table id="coursetable"></table>`
         );
-        Tablestatus=0;
+        Tablestatus = 0;
     }
-    //  $("#coursetable").remove();
     setnewdate($("#month").val(), $("#day").val());
     $("#coursetable").append("<tr><th>場次</th><th>時間</th><th>主題</th></tr>")
-    Tablestatus=1;
+    Tablestatus = 1;
     var topicCount = topic.length;
     //debugger;
     var milliseconds = 24 * 60 * 60 * 1000;
