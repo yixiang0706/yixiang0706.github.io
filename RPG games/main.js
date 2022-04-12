9af2d0b580e9200e7d983287fef2d570536ec3a6
yixiang0706 <97888369+yixiang0706@users.noreply.github.com>
2022-04-13 02:16:37 +0800

    NEWNEW
    NEWNEW

--- RPG games/main.js
+++ RPG games/main.js
@@ -115,7 +115,12 @@ $(document).on("keydown",function(event){
                 $("#talkBox").text("有山");
                 break;
             case 2: 
-                $("#talkBox").text("抵達終點");
+                $("#talkBox").text("抵達終點 聽歌休息一下吧!!");
+                document.querySelector('.YT').innerHTML = `<iframe width="700" height="470"
+                src="https://www.youtube.com/embed/SwZMS-ISbH4?autoplay=1">
+                </iframe><br><a href="./index.html">
+                <center><button class="shape-ex1">Again.</button></center></a>`;
+                YT.show();
                 currentImgMain.x = targetImg.x;
                 currentImgMain.y = targetImg.y;
                 break;
