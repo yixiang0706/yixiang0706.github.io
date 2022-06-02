e244617d84307d8b379d9ef5e56bcb26cc0cad48
yixiang0706 <97888369+yixiang0706@users.noreply.github.com>
2022-06-03 01:38:53 +0800

    d3

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,14 +42,6 @@ $(function(){
                 );//第二欄
                 $("#weathertable").append(
                     `<tr>
-                        <th>降水機率</br>(${data.records.location[0].weatherElement[1].elementName})</th>
-                        <th>${data.records.location[0].weatherElement[1].time[0].parameter.parameterName}%</th>
-                        <th>${data.records.location[0].weatherElement[1].time[1].parameter.parameterName}%</th>
-                        <th>${data.records.location[0].weatherElement[1].time[2].parameter.parameterName}%</th>
-                    </tr>`
-                );//第二欄
-                $("#weathertable").append(
-                    `<tr>
                         <th>最低溫度</br>(${data.records.location[0].weatherElement[2].elementName})</th>
                         <th>${data.records.location[0].weatherElement[2].time[0].parameter.parameterName}°C</th>
                         <th>${data.records.location[0].weatherElement[2].time[1].parameter.parameterName}°C</th>
@@ -72,7 +64,142 @@ $(function(){
                         <th>${data.records.location[0].weatherElement[3].time[2].parameter.parameterName}</th>
                     </tr>`
                 );//第四欄
+                $("#weathertable").append(
+                    `<tr>
+                        <th>降雨機率</br>(${data.records.location[0].weatherElement[1].elementName})</th>
+                        <th id="th1"></th>
+                        <th id="th2"></th>
+                        <th id="th3"></th>
+                    </tr>`
+                );
+
+
+                const width = 250;
+                const height = 250;
+
+                const svg1 = d3.select('#th1').append('svg').attr('width', width).attr('height', height);
+                const rootLayer1 = svg1.append('g');
+                const pieChartLayer1 = rootLayer1.append('g').attr('transform', `translate(${width / 2}, ${height / 2})`);
+            
+                const arc1 = d3.arc()
+                    .innerRadius(75)
+                    .outerRadius(100);
+
+                // 資料轉為角度資料
+                const data1 = [
+                    data.records.location[0].weatherElement[1].time[0].parameter.parameterName,
+                    100-data.records.location[0].weatherElement[1].time[0].parameter.parameterName
+                ];
+                const arcs1 = d3.pie()(data1);
+                var color = d3.scaleOrdinal(d3.schemeCategory10);  
+                // 使用`enter`新增`path`元件並設定繪製圖屬性`d`。
+                pieChartLayer1
+                    .selectAll('path')
+                    .data(arcs1)
+                    .enter()
+                    .append('path')
+                    .attr('d', data1 =>  arc1(data1))
+                    .attr('fill', function(d, i) {   
+                        return color(i);   
+                    })
+                    .attr('stroke','black')
+                    .attr('stroke-width','2px')
+
+
+                pieChartLayer1
+                .append('g')
+                .attr('class', 'text-group')
+
+                pieChartLayer1.select('g').append('text')
+                .attr('class', 'value-text')
+                .text(`降雨機率 ${data1[0]}%`)
+                .attr('text-anchor', 'middle')
+                .attr('dy', '.6em')
+                
+
+
+                //第二個-----------------------------------------------------------------------------------------------
+                const svg2 = d3.select('#th2').append('svg').attr('width', width).attr('height', height);
+                const rootLayer2 = svg2.append('g');
+                const pieChartLayer2 = rootLayer2.append('g').attr('transform', `translate(${width / 2}, ${height / 2})`);
+            
+                const arc2 = d3.arc()
+                    .innerRadius(75)
+                    .outerRadius(100);
+
+                // 資料轉為角度資料
+                const data2 = [
+                    data.records.location[0].weatherElement[1].time[1].parameter.parameterName,
+                    100-data.records.location[0].weatherElement[1].time[1].parameter.parameterName
+                ];
+                const arcs2 = d3.pie()(data2);
+                // 使用`enter`新增`path`元件並設定繪製圖屬性`d`。
+                pieChartLayer2
+                    .selectAll('path')
+                    .data(arcs2)
+                    .enter()
+                    .append('path')
+                    .attr('d', data2 =>  arc2(data2))
+                    .attr('fill', function(d, i) {   
+                        return color(i);   
+                    })
+                    .attr('stroke','black')
+                    .attr('stroke-width','2px')
+
+
+                pieChartLayer2
+                .append('g')
+                .attr('class', 'text-group')
+
+                pieChartLayer2.select('g').append('text')
+                .attr('class', 'value-text')
+                .text(`降雨機率 ${data2[0]}%`)
+                .attr('text-anchor', 'middle')
+                .attr('dy', '.6em')
+
+
+
+
+                //第三個-----------------------------------------------------------------------------------------------
+                const svg3 = d3.select('#th3').append('svg').attr('width', width).attr('height', height);
+                const rootLayer3 = svg3.append('g');
+                const pieChartLayer3 = rootLayer3.append('g').attr('transform', `translate(${width / 2}, ${height / 2})`);
+            
+                const arc3 = d3.arc()
+                    .innerRadius(75)
+                    .outerRadius(100);
+
+                // 資料轉為角度資料
+                const data3 = [
+                    data.records.location[0].weatherElement[1].time[2].parameter.parameterName,
+                    100-data.records.location[0].weatherElement[1].time[2].parameter.parameterName
+                ];
+                const arcs3 = d3.pie()(data3);
+                // 使用`enter`新增`path`元件並設定繪製圖屬性`d`。
+                pieChartLayer3
+                    .selectAll('path')
+                    .data(arcs3)
+                    .enter()
+                    .append('path')
+                    .attr('d', data3 =>  arc3(data3))
+                    .attr('fill', function(d, i) {   
+                        return color(i);   
+                    })
+                    .attr('stroke','black')
+                    .attr('stroke-width','2px')
+
+
+                pieChartLayer3
+                .append('g')
+                .attr('class', 'text-group')
+
+                pieChartLayer3.select('g').append('text')
+                .attr('class', 'value-text')
+                .text(`降雨機率 ${data3[0]}%`)
+                .attr('text-anchor', 'middle')
+                .attr('dy', '.6em')
             }
-        )
+         )
     }));
 });
+
